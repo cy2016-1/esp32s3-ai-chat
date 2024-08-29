fe65c34412bd4c1011a6d73353405e45f614fa30
chging <970818171@qq.com>
2024-08-29 15:04:41 +0800

    增加example及library文件

--- example/wake_detect/wake_detect.ino
+++ example/wake_detect/wake_detect.ino
@@ -1,125 +1,289 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_wn_iface.h"
-#include "esp_wn_models.h"
-#include "esp_afe_sr_models.h"
-#include "esp_mn_iface.h"
-#include "esp_mn_models.h"
-#include "esp_board_init.h"
-#include "model_path.h"
-#include "string.h"
+#include <xiao_ming_tong_xue_inferencing.h>
 
-int detect_flag = 0;
-static esp_afe_sr_iface_t *afe_handle = NULL;
-static esp_afe_sr_data_t *afe_data = NULL;
-static volatile int task_flag = 0;
+/* Edge Impulse Arduino examples
+ * Copyright (c) 2022 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-void feed_Task(void *arg)
-{
-    esp_afe_sr_data_t *afe_data = arg;
-    int audio_chunksize = afe_handle->get_feed_chunksize(afe_data);
-    int nch = afe_handle->get_channel_num(afe_data);
-    int feed_channel = esp_get_feed_channel();
-    assert(nch<feed_channel);
-    int16_t *i2s_buff = malloc(audio_chunksize * sizeof(int16_t) * feed_channel);
-    assert(i2s_buff);
+// If your target is limited in memory remove this macro to save 10K RAM
+#define EIDSP_QUANTIZE_FILTERBANK 0
 
-    while (task_flag) {
-        esp_get_feed_data(false, i2s_buff, audio_chunksize * sizeof(int16_t) * feed_channel);
+/*
+ ** NOTE: If you run into TFLite arena allocation issue.
+ **
+ ** This may be due to may dynamic memory fragmentation.
+ ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
+ ** if it doesn't exist) and copy this file to
+ ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
+ **
+ ** See
+ ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
+ ** to find where Arduino installs cores on your machine.
+ **
+ ** If the problem persists then there's not enough memory for this model and application.
+ */
 
-        afe_handle->feed(afe_data, i2s_buff);
-    }
-    if (i2s_buff) {
-        free(i2s_buff);
-        i2s_buff = NULL;
-    }
-    vTaskDelete(NULL);
-}
+/* Includes ---------------------------------------------------------------- */
+//#include <wakeup_detect_houguotongxue_inferencing.h>
 
-void detect_Task(void *arg)
-{
-    esp_afe_sr_data_t *afe_data = arg;
-    int afe_chunksize = afe_handle->get_fetch_chunksize(afe_data);
-    int16_t *buff = malloc(afe_chunksize * sizeof(int16_t));
-    assert(buff);
-    printf("------------detect start------------\n");
+#include <driver/i2s.h>
 
-    while (task_flag) {
-        afe_fetch_result_t* res = afe_handle->fetch(afe_data); 
-        if (!res || res->ret_value == ESP_FAIL) {
-            printf("fetch error!\n");
-            break;
-        }
+#define SAMPLE_RATE 16000U
+#define LED_BUILT_IN 21
 
-        if (res->wakeup_state == WAKENET_DETECTED) {
-            printf("wakeword detected\n");
-	        printf("model index:%d, word index:%d\n", res->wakenet_model_index, res->wake_word_index);
-            printf("-----------LISTENING-----------\n");
-        }
-    }
-    if (buff) {
-        free(buff);
-        buff = NULL;
-    }
-    vTaskDelete(NULL);
-}
+// INMP441 config
+#define I2S_IN_PORT I2S_NUM_0
+#define I2S_IN_BCLK 4
+#define I2S_IN_LRC 5
+#define I2S_IN_DIN 6
 
-void app_main()
-{
-    ESP_ERROR_CHECK(esp_board_init(16000, 1, 16));
-    // ESP_ERROR_CHECK(esp_sdcard_init("/sdcard", 10));
+/** Audio buffers, pointers and selectors */
+typedef struct {
+  int16_t *buffer;
+  uint8_t buf_ready;
+  uint32_t buf_count;
+  uint32_t n_samples;
+} inference_t;
 
-    srmodel_list_t *models = esp_srmodel_init("model");
-    char *wn_name = NULL;
-    char *wn_name_2 = NULL;
+static inference_t inference;
+static const uint32_t sample_buffer_size = 2048;
+static signed short sampleBuffer[sample_buffer_size];
+static bool debug_nn = false;  // Set this to true to see e.g. features generated from the raw signal
+static bool record_status = true;
 
-    if (models!=NULL) {
-        for (int i=0; i<models->num; i++) {
-            if (strstr(models->model_name[i], ESP_WN_PREFIX) != NULL) {
-                if (wn_name == NULL) {
-                    wn_name = models->model_name[i];
-                    printf("The first wakenet model: %s\n", wn_name);
-                } else if (wn_name_2 == NULL) {
-                    wn_name_2 = models->model_name[i];
-                    printf("The second wakenet model: %s\n", wn_name_2);
-                }
-            }
-        }
-    } else {
-        printf("Please enable wakenet model and select wake word by menuconfig!\n");
-        return ;
-    }
-
-    afe_handle = (esp_afe_sr_iface_t *)&ESP_AFE_SR_HANDLE;
-    afe_config_t afe_config = AFE_CONFIG_DEFAULT();
-    afe_config.memory_alloc_mode = AFE_MEMORY_ALLOC_MORE_PSRAM;
-    afe_config.wakenet_init = true;
-    afe_config.wakenet_model_name = wn_name;
-    afe_config.wakenet_model_name_2 = wn_name_2;
-    afe_config.voice_communication_init = false;
-
-#if CONFIG_ESP32_S3_EYE_BOARD || CONFIG_ESP32_P4_FUNCTION_EV_BOARD
-    afe_config.pcm_config.total_ch_num = 2;
-    afe_config.pcm_config.mic_num = 1;
-    afe_config.pcm_config.ref_num = 1;
-    afe_config.wakenet_mode = DET_MODE_90;
-    afe_config.se_init = false;
-#endif
-    afe_data = afe_handle->create_from_config(&afe_config);
-    
-    task_flag = 1;
-    xTaskCreatePinnedToCore(&feed_Task, "feed", 8 * 1024, (void*)afe_data, 5, NULL, 0);
-    xTaskCreatePinnedToCore(&detect_Task, "detect", 4 * 1024, (void*)afe_data, 5, NULL, 1);
-}
-
+/**
+ * @brief      Arduino setup function
+ */
 void setup() {
   // put your setup code here, to run once:
-  app_main();
+  Serial.begin(115200);
+  // comment out the below line to cancel the wait for USB connection (needed for native USB)
+  while (!Serial)
+    ;
+  Serial.println("Edge Impulse Inferencing Demo");
+
+  pinMode(LED_BUILT_IN, OUTPUT);     // Set the pin as output
+  digitalWrite(LED_BUILT_IN, HIGH);  //Turn off
+
+  // Initialize I2S for audio input
+  i2s_config_t i2s_config_in = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,  // 注意：INMP441 输出 32 位数据
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 8,
+    .dma_buf_len = 1024,
+  };
+  i2s_pin_config_t pin_config_in = {
+    .bck_io_num = I2S_IN_BCLK,
+    .ws_io_num = I2S_IN_LRC,
+    .data_out_num = -1,
+    .data_in_num = I2S_IN_DIN
+  };
+  i2s_driver_install(I2S_IN_PORT, &i2s_config_in, 0, NULL);
+  i2s_set_pin(I2S_IN_PORT, &pin_config_in);
+
+  // summary of inferencing settings (from model_metadata.h)
+  ei_printf("Inferencing settings:\n");
+  ei_printf("\tInterval: ");
+  ei_printf_float((float)EI_CLASSIFIER_INTERVAL_MS);
+  ei_printf(" ms.\n");
+  ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+  ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
+  ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) / sizeof(ei_classifier_inferencing_categories[0]));
+
+  ei_printf("\nStarting continious inference in 2 seconds...\n");
+  ei_sleep(2000);
+
+  if (microphone_inference_start(EI_CLASSIFIER_RAW_SAMPLE_COUNT) == false) {
+    ei_printf("ERR: Could not allocate audio buffer (size %d), this could be due to the window length of your model\r\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT);
+    return;
+  }
+
+  ei_printf("Recording...\n");
 }
 
+/**
+ * @brief      Arduino main function. Runs the inferencing loop.
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool m = microphone_inference_record();
+  if (!m) {
+    ei_printf("ERR: Failed to record audio...\n");
+    return;
+  }
 
+  signal_t signal;
+  signal.total_length = EI_CLASSIFIER_RAW_SAMPLE_COUNT;
+  signal.get_data = &microphone_audio_signal_get_data;
+  ei_impulse_result_t result = { 0 };
+
+  EI_IMPULSE_ERROR r = run_classifier(&signal, &result, debug_nn);
+  if (r != EI_IMPULSE_OK) {
+    ei_printf("ERR: Failed to run classifier (%d)\n", r);
+    return;
+  }
+
+  int pred_index = 0;    // Initialize pred_index
+  float pred_value = 0;  // Initialize pred_value
+
+  // print the predictions
+  ei_printf("Predictions ");
+  ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+  ei_printf(": \n");
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    ei_printf("    %s: ", result.classification[ix].label);
+    ei_printf_float(result.classification[ix].value);
+    ei_printf("\n");
+
+    if (result.classification[ix].value > pred_value) {
+      pred_index = ix;
+      pred_value = result.classification[ix].value;
+    }
+  }
+  // Display inference result
+  if (pred_index == 3) {
+    digitalWrite(LED_BUILT_IN, LOW);  //Turn on
+  } else {
+    digitalWrite(LED_BUILT_IN, HIGH);  //Turn off
+  }
+
+
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+  ei_printf("    anomaly score: ");
+  ei_printf_float(result.anomaly);
+  ei_printf("\n");
+#endif
 }
+
+static void audio_inference_callback(uint32_t n_bytes) {
+  for (int i = 0; i < n_bytes >> 1; i++) {
+    inference.buffer[inference.buf_count++] = sampleBuffer[i];
+
+    if (inference.buf_count >= inference.n_samples) {
+      inference.buf_count = 0;
+      inference.buf_ready = 1;
+    }
+  }
+}
+
+static void capture_samples(void *arg) {
+
+  const int32_t i2s_bytes_to_read = (uint32_t)arg;
+  size_t bytes_read = i2s_bytes_to_read;
+
+  while (record_status) {
+
+    /* read data at once from i2s - Modified for XIAO ESP2S3 Sense and I2S.h library */
+    i2s_read(I2S_IN_PORT, (void*)sampleBuffer, i2s_bytes_to_read, &bytes_read, 100);
+    // esp_i2s::i2s_read(esp_i2s::I2S_NUM_0, (void *)sampleBuffer, i2s_bytes_to_read, &bytes_read, 100);
+
+    if (bytes_read <= 0) {
+      ei_printf("Error in I2S read : %d", bytes_read);
+    } else {
+      if (bytes_read < i2s_bytes_to_read) {
+        ei_printf("Partial I2S read");
+      }
+
+      // scale the data (otherwise the sound is too quiet)
+      for (int x = 0; x < i2s_bytes_to_read / 2; x++) {
+        sampleBuffer[x] = (int16_t)(sampleBuffer[x]) * 8;
+      }
+
+      if (record_status) {
+        audio_inference_callback(i2s_bytes_to_read);
+      } else {
+        break;
+      }
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+/**
+ * @brief      Init inferencing struct and setup/start PDM
+ *
+ * @param[in]  n_samples  The n samples
+ *
+ * @return     { description_of_the_return_value }
+ */
+static bool microphone_inference_start(uint32_t n_samples) {
+  inference.buffer = (int16_t *)malloc(n_samples * sizeof(int16_t));
+
+  if (inference.buffer == NULL) {
+    return false;
+  }
+
+  inference.buf_count = 0;
+  inference.n_samples = n_samples;
+  inference.buf_ready = 0;
+
+  //    if (i2s_init(EI_CLASSIFIER_FREQUENCY)) {
+  //        ei_printf("Failed to start I2S!");
+  //    }
+
+  ei_sleep(100);
+
+  record_status = true;
+
+  xTaskCreate(capture_samples, "CaptureSamples", 1024 * 32, (void *)sample_buffer_size, 10, NULL);
+
+  return true;
+}
+
+/**
+ * @brief      Wait on new data
+ *
+ * @return     True when finished
+ */
+static bool microphone_inference_record(void) {
+  bool ret = true;
+
+  while (inference.buf_ready == 0) {
+    delay(10);
+  }
+
+  inference.buf_ready = 0;
+  return ret;
+}
+
+/**
+ * Get raw audio signal data
+ */
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr) {
+  numpy::int16_to_float(&inference.buffer[offset], out_ptr, length);
+
+  return 0;
+}
+
+/**
+ * @brief      Stop PDM and release buffers
+ */
+static void microphone_inference_end(void) {
+  free(sampleBuffer);
+  ei_free(inference.buffer);
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
